7b3cdaa66f91868636807f92014c5cc8ed65f836
Tibor Udvari <tibor.udvari@icloud.com>
2023-03-02 17:55:49 +0100

    Audio fix

--- sketch.js
+++ sketch.js
@@ -12,21 +12,23 @@ let sounds = [];
 let soundFiles = ["good-job-josh.mp3"];
 
 let audioMap = {
-  "Hello, I'm Cheerio, your AI cheerleader!": "hello-im-cheerio-your-ai-cheerleader.mp3",
+  "Hello, I'm Cheerio, your AI cheerleader!":
+    "hello-im-cheerio-your-ai-cheerleader.mp3",
   "Let's get started!": "lets-get-started.mp3",
   "Show me something in 3 seconds": "show-me-something-in-3-seconds.mp3",
-  "1": "1.mp3",
-  "2": "2.mp3",
-  "3": "3.mp3",
-  "Great, great! Now don't show me anything": "great-great-now-dont-show-me-anything.mp3",
+  1: "1.mp3",
+  2: "2.mp3",
+  3: "3.mp3",
+  "Great, great! Now don't show me anything":
+    "great-great-now-dont-show-me-anything.mp3",
   "Show me nothing! Go!": "show-me-nothing-go.mp3",
   "Keep going!": "keep-going.mp3",
-  "BOOM": "boom.mp3",
+  BOOM: "boom.mp3",
   "We're starting in 3!!!": "were-starting-in-3.mp3",
 };
 
 // CONFETTI: Pass in the id of an element
-let confetti = new Confetti('startBtn');
+let confetti = new Confetti("startBtn");
 
 // Edit given parameters
 confetti.setCount(150);
@@ -35,58 +37,65 @@ confetti.setPower(100);
 confetti.setFade(true);
 confetti.destroyTarget(false);
 
-
 function preload() {
-  for (let i = 0; i < soundFiles.length; i++) {
-    sounds[i] = loadSound('assets/' + soundFiles[i]);
-  }
+  audioMap = Object.assign(
+    {},
+    ...Object.keys(audioMap).map((key) => ({
+      [key]: loadSound("assets/audio/" + audioMap[key]),
+    }))
+  );
+  console.log(audioMap);
+
+  // Object.keys(audioMap).map(v => ({
+  //   fn: v,
+  //   sound: loadSound('assets/audio/' + v)
+  // }));
 }
 
 function voiceReady() {
   console.log(speech.voices);
-  speech.setVoice('Daniel');
-  speech.setLang('en-US');
-
+  speech.setVoice("Daniel");
+  speech.setLang("en-US");
 }
 
 function setup() {
-  reactEmoji = document.getElementById('react-emoji');
+  reactEmoji = document.getElementById("react-emoji");
 
-
-  sound = loadSound('assets/good-job-josh.mp3');
+  sound = loadSound("assets/good-job-josh.mp3");
   speech = new p5.Speech(voiceReady); //callback, speech synthesis object
 
-  featureExtractor = ml5.featureExtractor('MobileNet', modelReady);
+  featureExtractor = ml5.featureExtractor("MobileNet", modelReady);
   noCanvas();
   video = createCapture(VIDEO);
-  video.parent('videoContainer');
-  
-  let bgBtn = document.querySelector('#bgBtn');
-  let onBtn = document.querySelector('#onBtn');
-  let offBtn = document.querySelector('#offBtn');
-  let runBtn = document.querySelector('#runBtn');
-  let clearBtn = document.querySelector('#clearBtn');
-  let startBtn = document.querySelector('#startBtn');
+  video.parent("videoContainer");
 
-  startBtn.addEventListener('click', startExperience);
+  let bgBtn = document.querySelector("#bgBtn");
+  let onBtn = document.querySelector("#onBtn");
+  let offBtn = document.querySelector("#offBtn");
+  let runBtn = document.querySelector("#runBtn");
+  let clearBtn = document.querySelector("#clearBtn");
+  let startBtn = document.querySelector("#startBtn");
 
-  bgBtn.addEventListener('click', () => {
-    addExample('bg');
+  startBtn.addEventListener("click", startExperience);
+
+  bgBtn.addEventListener("click", () => {
+    addExample("bg");
   });
-  
-  onBtn.addEventListener('click', () => {
-    addExample('on');
+
+  onBtn.addEventListener("click", () => {
+    addExample("on");
   });
-  
-  offBtn.addEventListener('click', () => {
-    addExample('off');
+
+  offBtn.addEventListener("click", () => {
+    addExample("off");
   });
-  
-  runBtn.addEventListener('click', classify);
-  clearBtn.addEventListener('click', clearAllLabels);
+
+  runBtn.addEventListener("click", classify);
+  clearBtn.addEventListener("click", clearAllLabels);
 }
 
-function modelReady(){  select('#status').html('FeatureExtractorfleNet model) Loaded')
+function modelReady() {
+  select("#status").html("FeatureExtractorfleNet model) Loaded");
 }
 
 function addExample(label) {
@@ -98,7 +107,7 @@ function addExample(label) {
 function classify() {
   const numLabels = knnClassifier.getNumLabels();
   if (numLabels <= 0) {
-    console.error('There is no examples in any label');
+    console.error("There is no examples in any label");
     return;
   }
   const features = featureExtractor.infer(video);
@@ -114,36 +123,22 @@ function gotResults(err, result) {
     const confidences = result.confidencesByLabel;
     if (result.label) {
       //console.log(result);
-      select('#result').html(result.label);
-      select('#confidence').html(`${confidences[result.label] * 100} %`);
-      
-      if (confidences[result.label] > confTreshold) {        
+      select("#result").html(result.label);
+      select("#confidence").html(`${confidences[result.label] * 100} %`);
+
+      if (confidences[result.label] > confTreshold) {
         cLabel = result.label;
-        
-        if (cLabel == 'on' && pLabel == 'off') {
+
+        if (cLabel == "on" && pLabel == "off") {
           //console.log("trigger");
           sound.play();
         }
-        
+
         pLabel = cLabel;
       }
-      
     }
   }
-  
-  // confidence
-  /*
-    classIndex: 0
-    label: "bg"
-    confidences: Object
-      0: 0.6666666666666666
-      1: 0
-      2: 0.3333333333333333
-    confidencesByLabel: Object
-    bg: 0.6666666666666666
-    on: 0
-    off: 0.3333333333333333
-  */
+
   classify();
 }
 
@@ -151,13 +146,16 @@ async function say(something) {
   console.log(something);
   // load and play a sound file if exits
   if (audioMap[something]) {
-    let audio = new Audio('./assets/' + audioMap[something]);
-    audio.play();
-    await new Promise(resolve => audio.ended(resolve));
+    audioMap[something].play();
+    await new Promise((resolve) => audioMap[something].onended(resolve));
+
+    // let audio = new Audio('./assets/audio/' + audioMap[something]);
+    // audio.play();
+    // await new Promise(resolve => audio.ended(resolve));
   } else {
-    speech.speak(something); 
+    speech.speak(something);
     console.error("No audio file for " + something);
-    await new Promise(resolve => speech.ended(resolve));
+    await new Promise((resolve) => speech.ended(resolve));
   }
 }
 
@@ -165,8 +163,10 @@ async function startExperience() {
   // reactEmoji.classList.add("animate__animated");
   // reactEmoji.classList.add("animate__bounce");
 
-  reactEmoji.src = await new Promise(resolve => resolve("./assets/explosion.png"))
-  
+  reactEmoji.src = await new Promise((resolve) =>
+    resolve("./assets/explosion.png")
+  );
+
   await say("Hello, I'm Cheerio, your AI cheerleader!");
   await wait(100);
 
@@ -176,28 +176,27 @@ async function startExperience() {
   await say("Show me something in 3 seconds");
   await wait(100);
 
-  reactEmoji.src = await new Promise(resolve => resolve("./assets/3.png")); 
-  
+  reactEmoji.src = await new Promise((resolve) => resolve("./assets/3.png"));
+
   await say("3");
   await wait(100);
 
-  reactEmoji.src = await new Promise(resolve => resolve("./assets/2.png" ));
-  
+  reactEmoji.src = await new Promise((resolve) => resolve("./assets/2.png"));
+
   await say("2");
   await wait(100);
 
-  reactEmoji.src = await new Promise(resolve => resolve("./assets/1.png" ));
-  
+  reactEmoji.src = await new Promise((resolve) => resolve("./assets/1.png"));
+
   await say("1");
   await wait(100);
-
 
   await say("Show me something");
 
   for (var i = 0; i < 20; i++) {
     await wait(100);
-    addExample('on');
-    
+    addExample("on");
+
     if (Math.random() < 0.1) {
       await say("Keep going!");
     }
@@ -206,17 +205,17 @@ async function startExperience() {
   await say("Great, great! Now don't show me anything");
   await wait(100);
 
-  reactEmoji.src = await new Promise(resolve => resolve("./assets/3.png"));
+  reactEmoji.src = await new Promise((resolve) => resolve("./assets/3.png"));
   await say("3");
   await wait(100);
 
-  reactEmoji.src = await new Promise(resolve => resolve("./assets/2.png" ));
-  
+  reactEmoji.src = await new Promise((resolve) => resolve("./assets/2.png"));
+
   await say("2");
   await wait(100);
 
-  reactEmoji.src = await new Promise(resolve => resolve("./assets/1.png" ));
-  
+  reactEmoji.src = await new Promise((resolve) => resolve("./assets/1.png"));
+
   await say("1");
   await wait(100);
 
@@ -224,15 +223,15 @@ async function startExperience() {
 
   for (var i = 0; i < 20; i++) {
     await wait(100);
-    addExample('off');
-    
+    addExample("off");
+
     if (Math.random() < 0.1) {
       await say("Keep going!");
     }
   }
 
-  reactEmoji.src = await new Promise(resolve => resolve("./assets/100.png" ));
-  
+  reactEmoji.src = await new Promise((resolve) => resolve("./assets/100.png"));
+
   await say("BOOM");
 
   await say("We are good to go, chief!");
@@ -240,30 +239,26 @@ async function startExperience() {
   await say("We are starting in 3!!!");
   await wait(100);
 
-  reactEmoji.src = await new Promise(resolve => resolve("./assets/3.png"));
-  
+  reactEmoji.src = await new Promise((resolve) => resolve("./assets/3.png"));
+
   await say("3");
   await wait(100);
 
-  reactEmoji.src = await new Promise(resolve => resolve("./assets/2.png" ));
-  
+  reactEmoji.src = await new Promise((resolve) => resolve("./assets/2.png"));
+
   await say("2");
   await wait(100);
 
-  reactEmoji.src = await new Promise(resolve => resolve("./assets/1.png" ));
+  reactEmoji.src = await new Promise((resolve) => resolve("./assets/1.png"));
   await say("1");
   await wait(100);
 
-  reactEmoji.src = await new Promise(resolve => resolve("./assets/100.png" ));
+  reactEmoji.src = await new Promise((resolve) => resolve("./assets/100.png"));
   await say("BOOM");
-
 
   classify();
 
-
-
   // start the experience
-
 
   // Train bg class
   // Countdown
@@ -280,14 +275,13 @@ async function startExperience() {
 
   // Welcome message
 
-  await new Promise(resolve => sound.onended(resolve));
+  await new Promise((resolve) => sound.onended(resolve));
   console.log("sound finished playing");
-
 }
 
 function updateCounts() {
   const counts = knnClassifier.getCountByLabel();
-  select('#status').html(JSON.stringify(counts, null, 2));
+  select("#status").html(JSON.stringify(counts, null, 2));
 }
 
 function clearAllLabels() {
@@ -296,7 +290,7 @@ function clearAllLabels() {
 }
 
 function wait(milliseconds) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve();
     }, milliseconds);
